075649742cef21799db88a8fbcf5d088f2c47b75
Saulo Aldighieri Moraes <saulo @ gmx com>
2020-04-09 10:40:59 -0300

    + Day 7

---
+++ Day007/README.md
@@ -0,0 +1,7 @@
+Day : 7/100
+Task: Position from browser
+
+- Get your position from your browser in the form of latitude and longitude  
+**Hint check out our blog posts 
+Winking face
+ at http://developer.here.com/blog
---
+++ Day007/js/index.js
@@ -0,0 +1,34 @@
+var platform = new H.service.Platform({
+    apikey: 'THE_KEY'
+  });
+
+// Obtain the default map types from the platform object:
+var defaultLayers = platform.createDefaultLayers();
+
+// Instantiate (and display) a map object:
+var map = new H.Map(
+    document.getElementById('mapContainer'),
+    defaultLayers.vector.normal.map,
+    {
+      zoom: 13,
+      center: { lat: -22.7617, lng: -47.1541 },
+      pixelRatio: window.devicePixelRatio || 1
+    });
+
+var ui = H.ui.UI.createDefault(map, defaultLayers);
+
+var mapEvents = new H.mapevents.MapEvents(map);
+
+var behavior = new H.mapevents.Behavior(mapEvents);
+
+// Get an instance of the geocoding service:
+var service = platform.getSearchService();
+
+service.geocode({
+  q: 'Paulínia SP'
+}, (result) => {
+  // Add a marker for each location found
+  result.items.forEach((item) => {
+    map.addObject(new H.map.Marker(item.position));
+  });
+}, alert);
